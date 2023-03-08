23f489e932e7a73b884870b8c91ac7f8640261e0
krystianpis <krystianp791@gmail.com>
2023-03-08 19:15:05 +0100

    added section gallery, contact, added styles in footer, added js to navigation on mobile

--- js/main.js
+++ js/main.js
@@ -5,9 +5,17 @@ const footerYear = document.querySelector('.footer__year')
 const handleNav = () => {
 	navBtn.classList.toggle('is-active')
 	navMobile.classList.toggle('nav-mobile--active')
+	document.body.classList.toggle('sticky-body')
+}
+
+const removeSticky = () => {
+	navBtn.classList.remove('is-active')
+	navMobile.classList.remove('nav-mobile--active')
+	document.body.classList.remove('sticky-body')
 }
 
 navBtn.addEventListener('click', handleNav)
+navMobile.addEventListener('click', removeSticky)
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
